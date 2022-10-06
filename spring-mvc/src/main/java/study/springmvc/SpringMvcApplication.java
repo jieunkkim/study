5995173e5aed5fad94c3c5aaa7409e53c92b63fb
jieunkkim <wldmsidqwew@gmail.com>
2022-10-07 01:11:28 +0900

    set main

---
+++ spring-mvc/src/main/java/study/springmvc/SpringMvcApplication.java
@@ -0,0 +1,14 @@
+package study.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class SpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcApplication.class, args);
+    }
+}
